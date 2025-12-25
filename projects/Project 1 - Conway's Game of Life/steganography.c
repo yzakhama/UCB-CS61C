9fcbe2806dcc55ddd1ff236a966575bdb63df73c
Yassine Zakhama <yzakhama@gmail.com>
2025-12-25 18:15:12 +0100

    Proj1: implement decoding logic

--- projects/Project 1 - Conway's Game of Life/steganography.c
+++ projects/Project 1 - Conway's Game of Life/steganography.c
@@ -18,15 +18,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned get_bit(unsigned x, unsigned n) {
+	return ((1 << n) & x) >> n;
+}
+
 // Determines what color the cell at the given row/col should be. This should
 // not affect Image, and should allocate space for a new Color.
 Color* evaluateOnePixel(Image* image, int row, int col) {
-	// YOUR CODE HERE
+	uint8_t b = image->image[row][col].B;
+	if (get_bit(b, 0)) {
+		Color* white = malloc(sizeof(Color));
+		white->R = white->G = white->B = 255;
+		return white;
+
+	} else {
+		Color* black = malloc(sizeof(Color));
+		black->R = black->G = black->B = 0;
+		return black;
+	}
 }
 
 // Given an image, creates a new image extracting the LSB of the B channel.
 Image* steganography(Image* image) {
-	// YOUR CODE HERE
+	Image* new_image = malloc(sizeof(Image));
+	new_image->cols = image->cols;
+	new_image->rows = image->rows;
+	new_image->image = malloc(image->rows * sizeof(Color*));
+	for (int i = 0; i < image->rows; ++i) {
+		new_image->image[i] = malloc(image->cols * sizeof(Color));
+
+		for (int j = 0; j < image->cols; ++j) {
+			Color* color = evaluateOnePixel(image, i, j);
+			new_image->image[i][j] = *color;
+			free(color);
+		}
+	}
+	return new_image;
 }
 
 /*
@@ -44,6 +71,10 @@ should return from main with code 0. Make sure to free all memory before
 returning!
 */
 int main(int argc, char** argv) {
-	// YOUR CODE HERE
-	Image* image = readData("./testInputs/blinkerH.ppm");
+	Image* image = readData(argv[1]);
+	Image* decoded_image = steganography(image);
+	writeData(decoded_image);
+	freeImage(image);
+	freeImage(decoded_image);
+	return 0;
 }
